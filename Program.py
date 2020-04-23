9ceb1776727699b7cc365525c49f07011e81c298
Ammar sahyoun <ammarzgr@hotmail.com>
2020-04-23 23:01:35 +0200

    create board dictionary

--- Program.py
+++ Program.py
@@ -1,1 +1,17 @@
 print('---- Welcom to TicTacToe version 1,0 -----')
+
+print('7|8|9')
+print('-+-+-')
+print('4|5|6')
+print('-+-+-')
+print('1|2|3')
+print('***********************************')
+
+board = {
+    '7': ' ', '8': ' ', '9': ' ',
+    '4': ' ', '5': ' ', '6': ' ',
+    '1': ' ', '2': ' ', '3': ' '
+}
+player = 1  # first player
+total_moves = 0  # counts of moves
+end_check = 0
