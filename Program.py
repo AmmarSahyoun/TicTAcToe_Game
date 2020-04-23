0661d6cd09fb8155a7934e8e3ef5e1e8494200f7
Ammar sahyoun <ammarzgr@hotmail.com>
2020-04-23 23:23:58 +0200

    test and debug. Leave DevNotes

--- Program.py
+++ Program.py
@@ -16,37 +16,6 @@ player = 1  # first player
 total_moves = 0  # counts of moves
 end_check = 0
 
-while True:
-    print(board['7'] + '|' + board['8'] + '|' + board['9'])
-    print('-+-+-')
-    print(board['4'] + '|' + board['5'] + '|' + board['6'])
-    print('-+-+-')
-    print(board['1'] + '|' + board['2'] + '|' + board['3'])
-
-    if total_moves == 9 or end_check == 1:
-        break
-    while True:  # input from player
-        if player == 1:  # choose player
-            p1_input = input('player one:')
-            if p1_input.upper() in board and board[p1_input.upper()] == ' ':
-                board[p1_input.upper()] = 'x'
-                player = 2
-                break
-            else:
-                print('invalid input, please try again')
-                continue
-        else:
-            p2_input = input('player two:')
-            if p2_input.upper() in board and board[p2_input.upper()] == ' ':
-                board[p2_input.upper()] = 'O'
-                player = 1
-                break
-            else:
-                print('invalid input, please try again')
-                continue
-    total_moves += 1
-    print('***********************************')
-
 
 def check():
     # check p1 moves
@@ -107,3 +76,39 @@ def check():
         print('******player one won!*******')
         return 1
     return 0
+
+
+while True:
+    print(board['7'] + '|' + board['8'] + '|' + board['9'])
+    print('-+-+-')
+    print(board['4'] + '|' + board['5'] + '|' + board['6'])
+    print('-+-+-')
+    print(board['1'] + '|' + board['2'] + '|' + board['3'])
+    end_check = check()
+    if total_moves == 9 or end_check == 1:
+        break
+    while True:  # input from player
+        if player == 1:  # choose player
+            p1_input = input('player one:')
+            if p1_input.upper() in board and board[p1_input.upper()] == ' ':
+                board[p1_input.upper()] = 'x'
+                player = 2
+                break
+            else:
+                print('invalid input, please try again')
+                continue
+        else:
+            p2_input = input('player two:')
+            if p2_input.upper() in board and board[p2_input.upper()] == ' ':
+                board[p2_input.upper()] = 'O'
+                player = 1
+                break
+            else:
+                print('invalid input, please try again')
+                continue
+    total_moves += 1
+    print('***********************************')
+
+#  Dev Note: need try catch the inputs
+#  Dev Note: need 2 draw the board by looping
+#  colorfull board
